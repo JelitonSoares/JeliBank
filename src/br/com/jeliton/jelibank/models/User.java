a9a5784725151c0d20c2d3bfd34f20b7d8e6a6f8
Jeliton Soares <jelitonn.soarres@gmail.com>
2023-07-29 14:19:09 -0300

    Modificações na classe User

--- src/br/com/jeliton/jelibank/models/User.java
+++ src/br/com/jeliton/jelibank/models/User.java
@@ -1,5 +1,7 @@
 package br.com.jeliton.jelibank.models;
 
+import java.util.Scanner;
+
 public class User {
     private String userName;
     private String accountType;
@@ -14,25 +16,74 @@ public class User {
         this.password = password;
     }
 
-    public double getSale () {
-        return sale;
+    public String getUserName () {
+        return userName;
     }
 
-    private double receiveValue (double value) {
+    public String getAccountType () {
+        return accountType;
+    }
+
+    public double getSale () {
+        return  sale;
+    }
+
+    private double completeReceive(double value) {
             sale += value;
             return sale;
     }
 
-    public  void completeTransfer (double value, String password) {
+    public void displayTechnicalSheet () {
+        System.out.println("""
+                ***********************
+                Ficha Tecnica do Cliente:
+                Nome: %s
+                Tipo de Conta: %s
+                Saldo Inicial: %.2f
+                ***********************
+                """.formatted(userName, accountType, sale));
+    }
+    public  void receiveValue(double value, String password) {
         if (password.equals(this.password)) {
-            double newSale = receiveValue(value);
+            double newSale = completeReceive(value);
             System.out.println(String.format("""
-                    Transferencia Concluída!!
+                    Valor Recebido!!
                     Novo Saldo: %.2f""", newSale));
         } else {
             System.out.println("Senha inválida!!");
         }
     }
+
+    public void trasferValue (double value, String password) {
+        if (password.equals(this.password)) {
+            double newSale = completeTransfer(value);
+            System.out.println("""
+                    Transferência Concluida!!
+                    Novo saldo: %.2f""".formatted(newSale));
+        } else {
+            System.out.println("Senha Inválida!!");
+
+        }
+    }
+
+    public double completeTransfer (double value) {
+        sale -= value;
+        return sale;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    public String pegaSenha () {
+        System.out.println("Digite a sua senha: ");
+        String senha = scanner.nextLine();
+        return senha;
+    }
+
+    public double pegaValor () {
+        System.out.println("Digite o valor: ");
+        double valor = scanner.nextDouble();
+        return valor;
+    }
+
 
 
 }
